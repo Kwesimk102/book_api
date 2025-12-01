bd289e8b1b5bb4b376cd3d6791a20ba5a5a96144
kwesimifetu <kwesimifetu102@gmail.com>
2025-12-01 12:29:13 +0000

    Fix Django errors:models,views, serializers and router imports

--- book_api/books/views.py
+++ book_api/books/views.py
@@ -4,5 +4,5 @@ from .serializers import BookSerializer
 
 # Create your views here.
 class BookViewSet(viewsets.ModelViewSet):
-queryset = Book.objects.all()
-serializer_class =BookSerializer
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
